7f849653f0acf39b7fb90334081f44db6a0fcf1b
Aphaki <kiapha1108@gmail.com>
2022-05-16 19:49:16 +0900

    Initial Commit

---
+++ MyFrirendList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFrirendList
+//
+//  Created by Sy Lee on 2022/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFrirendList/MyFrirendListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFrirendListApp.swift
+//  MyFrirendList
+//
+//  Created by Sy Lee on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct MyFrirendListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
